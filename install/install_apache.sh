fe89b40056dc9f6b83f08958766f10c2d3d688da
Rene Krenn <rkrenn@phoenixctms.org>
2022-03-07 16:53:49 +0100

    move http conf to config-default repo

--- install/install_apache.sh
+++ install/install_apache.sh
@@ -1,8 +1,8 @@
 ###setup apache2
 apt-get -y install apache2 libapache2-mod-jk libapache2-mod-fcgid
 usermod --append --groups tomcat,ctsms www-data
-wget https://raw.githubusercontent.com/phoenixctms/install-debian/$TAG/apache/00_ctsms_http.conf -O /etc/apache2/sites-available/00_ctsms_http.conf
-wget https://raw.githubusercontent.com/phoenixctms/install-debian/$TAG/apache/00_ctsms_https.conf -O /etc/apache2/sites-available/00_ctsms_https.conf
+cp /ctsms/install/00_ctsms_http.conf /etc/apache2/sites-available/00_ctsms_http.conf
+cp /ctsms/install/00_ctsms_https.conf /etc/apache2/sites-available/00_ctsms_https.conf
 cp /ctsms/install/ports.conf /etc/apache2/ports.conf
 wget https://raw.githubusercontent.com/phoenixctms/install-debian/$TAG/apache/blocklist.conf -O /etc/apache2/blocklist.conf
 wget https://raw.githubusercontent.com/phoenixctms/install-debian/$TAG/apache/jk.conf -O /etc/apache2/mods-available/jk.conf
