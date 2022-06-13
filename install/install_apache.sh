990598dd0958dbad9290938657cdda316392a2c1
Rene Krenn <rkrenn@phoenixctms.org>
2022-06-13 18:05:18 +0200

    silence download progress

--- install/install_apache.sh
+++ install/install_apache.sh
@@ -1,11 +1,11 @@
 ###setup apache2
-apt-get -y install apache2 libapache2-mod-jk libapache2-mod-fcgid
+apt-get -q -y install apache2 libapache2-mod-jk libapache2-mod-fcgid
 usermod --append --groups tomcat,ctsms www-data
 cp /ctsms/install/apache/00_ctsms_http.conf /etc/apache2/sites-available/00_ctsms_http.conf
 cp /ctsms/install/apache/00_ctsms_https.conf /etc/apache2/sites-available/00_ctsms_https.conf
 cp /ctsms/install/apache/ports.conf /etc/apache2/ports.conf
-wget https://raw.githubusercontent.com/phoenixctms/install-debian/$TAG/apache/blocklist.conf -O /etc/apache2/blocklist.conf
-wget https://raw.githubusercontent.com/phoenixctms/install-debian/$TAG/apache/jk.conf -O /etc/apache2/mods-available/jk.conf
+wget --no-verbose https://raw.githubusercontent.com/phoenixctms/install-debian/$TAG/apache/blocklist.conf -O /etc/apache2/blocklist.conf
+wget --no-verbose https://raw.githubusercontent.com/phoenixctms/install-debian/$TAG/apache/jk.conf -O /etc/apache2/mods-available/jk.conf
 a2dissite 000-default.conf
 a2ensite 00_ctsms_http.conf
 a2enmod ssl
